5c7516ea42b20cf40c7adcf4a7c884bf8716621b
shy <415502155@qq.com>
2019-10-21 10:28:20 +0800

    添加熔断器异常处理类

--- SpringCloudFeign/src/main/java/com/spring/cloud/feign/FeignService.java
+++ SpringCloudFeign/src/main/java/com/spring/cloud/feign/FeignService.java
@@ -1,10 +1,12 @@
-package com.spring.cloud.serviceinterface;
+package com.spring.cloud.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.cloud.hystrix.HystrixFallbackFactory;
 //@FeignClient(“服务名”)注解指定要调用的服务
-@FeignClient(name = "cloud-server")
+@FeignClient(name = "cloud-server", fallbackFactory = HystrixFallbackFactory.class)
 @Component
 public interface FeignService {
 	/***
