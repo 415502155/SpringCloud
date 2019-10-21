a90393137411e6d2c44baabcb8448f4e85aabfb5
shy <415502155@qq.com>
2019-10-21 10:40:52 +0800

    Zuul网关服务启动类

---
+++ SpringCloudZuul/src/main/java/com/spring/cloud/SpringCloudZuulApplication.java
@@ -0,0 +1,21 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.spring.cloud.filter.AccessFilter;
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringCloudZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulApplication.class, args);
+	}
+	
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
+	}
+}
