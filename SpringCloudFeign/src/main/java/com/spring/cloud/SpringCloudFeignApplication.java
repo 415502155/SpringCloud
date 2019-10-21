fa03e379422a092ba0be29d416966ffebd503e6b
shy <415502155@qq.com>
2019-10-21 10:31:16 +0800

    开启熔断器

--- SpringCloudFeign/src/main/java/com/spring/cloud/SpringCloudFeignApplication.java
+++ SpringCloudFeign/src/main/java/com/spring/cloud/SpringCloudFeignApplication.java
@@ -2,6 +2,7 @@ package com.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,6 +12,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 //开启Feign的功能
 @EnableFeignClients
+//断路器功能
+@EnableCircuitBreaker
 public class SpringCloudFeignApplication {
 
 	public static void main(String[] args) {
